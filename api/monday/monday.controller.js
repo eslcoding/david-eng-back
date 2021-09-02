916b84631e8abeb0da9d97ea0a81ddea425b616b
Stav Yaar Bar <anistu@gmail.com>
2021-09-02 15:38:01 +0300

    fix for heroku timeout

--- api/monday/monday.controller.js
+++ api/monday/monday.controller.js
@@ -65,6 +65,7 @@ async function getInter(req, res) {
 async function getInterTest(req, res) {
   // return res.end()
   if (global.isReqOn) return res.end()
+  global.isReqOn = true
 
   const body = req.body
   try {
